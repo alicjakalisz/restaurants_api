854fac756a15b04c14f83fe794ce5029d8d0ee08
Alicja Kalisz <alicja.kalisz@hotmail.co.uk>
2021-06-03 08:11:16 +0100

    commit -Maven resources compiler: Failed to copy '/home/dave/dev/workspace/apis/restaurants-api/src/test/resources' to '/home/dave/dev/workspace/apis/restaurants-api/target/test-classes/.': Couldn't copy [/home/dave/dev/workspace/apis/restaurants-api/src/test/resources] to [/home/dave/dev/workspace/apis/restaurants-api/target/test-classes/.]

---
+++ src/test/java/RestuarantServiceTest/RestaurantServiceTest.java
@@ -0,0 +1,53 @@
+package RestuarantServiceTest;
+
+
+import com.alicja.restaurants.json_converter.JsonConverter;
+import com.alicja.restaurants.service.RestaurantService;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import java.io.*;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ContextConfiguration
+public class RestaurantServiceTest {
+
+
+    @Mock
+    JsonConverter converter;
+
+    @InjectMocks
+    RestaurantService service;
+
+    @Test
+    public void testShouldReturnAListofRestaurants() throws IOException {
+        //You need to read as a String the file you placed in src/test/resources with the response from google.
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("src/test/resources")));
+        StringBuilder result = new StringBuilder();
+
+        String line;
+        while ((line = br.readLine()) != null) {
+            result.append(line);
+        }
+        br.close();
+        //json read into the string from resource file (you can do it directly from URL with URL class, then url.openconnection, conn. setGetrequest, conn.getInputStream into Buffer
+        String urlContentString = result.toString();
+         //Fill the response from the Buffer into a String
+        JsonParser jsonParser = new JsonParser();
+        JsonObject jsonObject = jsonParser.parse(urlContentString).getAsJsonObject();
+
+
+        when(converter.convertStringURLIntoJsonObject(urlContentString)).thenReturn(jsonObject);
+
+      //  Now you have a mock for the jsonConverter that is not really connecting to a URL. Any call to any URL always returns the result from the src/test/resources/google_response.json file.
+    }
+}
